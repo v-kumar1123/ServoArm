7de2ae74e15e0ba128d73805d4394328d6b72290
OTHSCS164 <OTHSCS164@KATYISD.ORG>
2020-03-06 13:32:25 -0600

    h

--- RobotArmCode/RobotArmCode.ino
+++ RobotArmCode/RobotArmCode.ino
@@ -1,9 +1,10 @@
 /*
- * HOW IT WORKS:
+ * GOALS:
+ * This arm should have a simplified control, with only one joystick, 
  * 
- */
+  */
 /*
- * Controlling a gripper arm with multiple servos- in the future, radio control is going to be added
+ * Controlling a gripper arm with multiple servos, with a radio. 
  */
  
 #include <Servo.h>
@@ -15,7 +16,12 @@ Servo gripper;
 
 void setup() {
   // put your setup code here, to run once:
-  rotatorBase.attach(10);//Digital port to which the rotator base servo is attached
+  rotatorBase.attach(10);//Digital port to which the rotator base servo is attached, so on, so forth
+  firstLeg.attach(9);
+  secondLeg.attach(8);
+  gripper.attach(11);
+
+  
 }
 
 void loop() {
